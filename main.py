9740aa6c70015f06b79d342ffde475897a4ebb5f
Ammar-Satouf <viberus512@gmail.com>
2025-06-12 16:52:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram.ext import AIORateLimiter
 from handlers import start, handle_message
 import os
-import asyncio
 
-PORT = int(os.environ.get("PORT", 8443))  # يستخدمه Render
-TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # رابط موقعك على Render (بصيغة https://your-app-name.onrender.com)
+async def on_startup(app):
+    webhook_url = os.getenv("WEBHOOK_URL")
+    await app.bot.set_webhook(url=webhook_url)
+    print("✅ تم إعداد الـ Webhook بنجاح!")
 
-async def main():
-    if not TOKEN or not WEBHOOK_URL:
-        print("⚠ تأكد من وجود BOT_TOKEN و WEBHOOK_URL في متغيرات البيئة")
+def main():
+    TOKEN = os.getenv("BOT_TOKEN")
+    if not TOKEN:
+        print("⚠ لم يتم العثور على التوكن في متغيرات البيئة")
         return
 
-    app = ApplicationBuilder().token(TOKEN).build()
+    app = ApplicationBuilder()\
+        .token(TOKEN)\
+        .rate_limiter(AIORateLimiter())\
+        .build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
 
-    await app.start()
-    await app.bot.set_webhook(f"{WEBHOOK_URL}/webhook")
-
-    print("✅ البوت يعمل باستخدام Webhook...")
-    await app.updater.start_webhook(
+    app.run_webhook(
         listen="0.0.0.0",
-        port=PORT,
-        url_path="webhook",
-        webhook_url=f"{WEBHOOK_URL}/webhook"
+        port=int(os.environ.get("PORT", 8080)),
+        webhook_url=os.getenv("WEBHOOK_URL"),
+        on_startup=on_startup
     )
 
-    await app.updater.idle()
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
